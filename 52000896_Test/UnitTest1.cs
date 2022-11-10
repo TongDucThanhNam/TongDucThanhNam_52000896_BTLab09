b5b9411e2b62949e322278c4953b8db3eb77450b
TeraSumi <TeraSumi@TERASUMI>
2022-11-10 10:27:05 +0700

    Add project files.

---
+++ 52000896_Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+using StudentServiceLib;
+using Xunit.Sdk;
+
+namespace _52000896_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_negetive_Should_return_Exception()
+        {
+            Student student = new Student();
+
+            student.Score = -1;
+
+            Assert.AreEqual(student.Score);
+
+        }
+
+        [TestMethod]
+        public void Add_1_Should_return_1()
+        {
+            Student student2 = new Student();
+
+            student2.Score = 1;
+
+            Assert.AreEqual(1, student2.Score);
+
+        }
+
+        [TestMethod]
+        public void Add_11_Should_return_Exeptcion()
+        {
+            Student student3 = new Student();
+
+            student3.Score = 1;
+
+            Assert.AreEqual(1, student3.Score);
+
+        }
+    }
+}
